dd4f0436d3da601cb38b5036922cf145cddebbde
Yuhri <matthewyuh246@gmail.com>
2025-03-06 22:04:56 +0900

    chapter11

---
+++ chapter11/foo/foo.go
@@ -0,0 +1,12 @@
+// Package foo provide functions for example.
+package foo
+
+// Add returns a plus b.
+func Add(a, b int) int {
+	return a + b
+}
+
+// Sub returns a minus b.
+func Sub(a, b int) int {
+	return a - b
+}
---
+++ chapter11/foo/go.mod
@@ -0,0 +1,3 @@
+module github.com/matthewyuh246/chapter11/foo
+
+go 1.22.3
